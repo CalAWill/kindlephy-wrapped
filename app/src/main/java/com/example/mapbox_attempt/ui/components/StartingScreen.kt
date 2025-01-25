0e58f0fdaa44f48ac5172210542150ac543f9529
Callum <callumwilson2209@outlook.com>
2025-01-25 21:13:07 +0000

    Initial commit

---
+++ app/src/main/java/com/example/mapbox_attempt/ui/components/StartingScreen.kt
@@ -0,0 +1,2 @@
+package com.example.mapbox_attempt.ui.components
+
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "mapbox_attempt"
+include(":app")
